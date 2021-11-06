6502a4ad4dd780af02046588b5a1aa115728dee7
RaulAstudillo06 <astudillo0606@gmail.com>
2021-11-06 15:48:42 -0400

    Increase noise level

--- bofn/utils/fit_gp_model.py
+++ bofn/utils/fit_gp_model.py
@@ -8,7 +8,7 @@ from gpytorch.mlls import ExactMarginalLogLikelihood
 def fit_gp_model(X, Y, Yvar=None):
     if Y.ndim == 1:
         Y = Y.unsqueeze(dim=-1)
-    model = FixedNoiseGP(X, Y, torch.ones(Y.shape) * 1e-5, outcome_transform=Standardize(m=Y.shape[-1]))
+    model = FixedNoiseGP(X, Y, torch.ones(Y.shape) * 1e-6, outcome_transform=Standardize(m=Y.shape[-1]))
     mll = ExactMarginalLogLikelihood(model.likelihood, model)
     fit_gpytorch_model(mll)
     return model
