318fe98096064b53b8349a32de991623af4988bf
RaulAstudillo06 <astudillo0606@gmail.com>
2024-11-10 13:13:35 -0800

    Try batch evaluations

--- bofn/acquisition_function_optimization/optimize_acqf.py
+++ bofn/acquisition_function_optimization/optimize_acqf.py
@@ -7,19 +7,17 @@ from botorch.optim.initializers import (
     gen_one_shot_kg_initial_conditions,
 )
 
-from bofn.acquisition_function_optimization.custom_acqf_optimizer import custom_optimize_acqf
-
 
 def optimize_acqf_and_get_suggested_point(
     acq_func,
     bounds,
     batch_size,
     posterior_mean=None,
-    ) -> Tensor:
+) -> Tensor:
     """Optimizes the acquisition function, and returns a new candidate."""
     input_dim = bounds.shape[1]
-    num_restarts=10*input_dim
-    raw_samples=100*input_dim
+    num_restarts = 10 * input_dim
+    raw_samples = 100 * input_dim
 
     ic_gen = (
         gen_one_shot_kg_initial_conditions
@@ -39,7 +37,7 @@ def optimize_acqf_and_get_suggested_point(
         baseline_candidate, _ = optimize_acqf(
             acq_function=posterior_mean,
             bounds=bounds,
-            q=batch_size,
+            q=1,
             num_restarts=num_restarts,
             raw_samples=raw_samples,
             options={"batch_limit": 5},
@@ -47,13 +45,19 @@ def optimize_acqf_and_get_suggested_point(
 
         if isinstance(acq_func, qKnowledgeGradient):
             augmented_q_batch_size = acq_func.get_augmented_q_batch_size(batch_size)
-            baseline_candidate = baseline_candidate.detach().repeat(1, augmented_q_batch_size, 1)
+            baseline_candidate = baseline_candidate.detach().repeat(
+                1, augmented_q_batch_size, 1
+            )
         else:
-            baseline_candidate = baseline_candidate.detach().view(torch.Size([1, batch_size, input_dim]))
-        
-        batch_initial_conditions = torch.cat([batch_initial_conditions, baseline_candidate], 0)
+            baseline_candidate = baseline_candidate.detach().view(
+                torch.Size([1, batch_size, input_dim])
+            )
+
+        batch_initial_conditions = torch.cat(
+            [batch_initial_conditions, baseline_candidate], 0
+        )
         num_restarts += 1
-    
+
     candidate, acq_value = optimize_acqf(
         acq_function=acq_func,
         bounds=bounds,
@@ -62,18 +66,18 @@ def optimize_acqf_and_get_suggested_point(
         raw_samples=raw_samples,
         batch_initial_conditions=batch_initial_conditions,
         options={"batch_limit": 2},
-        #options={'disp': True, 'iprint': 101},
+        # options={'disp': True, 'iprint': 101},
     )
 
     if baseline_candidate is not None:
         baseline_acq_value = acq_func.forward(baseline_candidate)[0].detach()
-        print('Test begins')
+        print("Test begins")
         print(acq_value)
         print(baseline_acq_value)
-        print('Test ends')
+        print("Test ends")
         if baseline_acq_value >= acq_value:
-            print('Baseline candidate was best found.')
+            print("Baseline candidate was best found.")
             candidate = baseline_candidate
-            
+
     new_x = candidate.detach().view([batch_size, input_dim])
     return new_x
--- bofn/bofn_trial.py
+++ bofn/bofn_trial.py
@@ -3,13 +3,19 @@ import os
 import sys
 import time
 import torch
-from botorch.acquisition import ExpectedImprovement, qExpectedImprovement, qKnowledgeGradient
+from botorch.acquisition import (
+    ExpectedImprovement,
+    qExpectedImprovement,
+    qKnowledgeGradient,
+)
 from botorch.acquisition import PosteriorMean as GPPosteriorMean
 from botorch.sampling.samplers import SobolQMCNormalSampler
 from torch import Tensor
 from typing import Callable, List, Optional
 
-from bofn.acquisition_function_optimization.optimize_acqf import optimize_acqf_and_get_suggested_point
+from bofn.acquisition_function_optimization.optimize_acqf import (
+    optimize_acqf_and_get_suggested_point,
+)
 from bofn.utils.dag import DAG
 from bofn.utils.initial_design import generate_initial_design
 from bofn.utils.fit_gp_model import fit_gp_model
@@ -20,7 +26,8 @@ from bofn.utils.posterior_mean import PosteriorMean
 def bofn_trial(
     problem: str,
     algo: str,
-    trial:int,
+    trial: int,
+    batch_size: int,
     n_init_evals: int,
     n_bo_iter: int,
     restart: bool,
@@ -38,18 +45,31 @@ def bofn_trial(
         # Check if training data is already available
         try:
             # Current available evaluations
-            X = torch.tensor(np.loadtxt(results_folder +
-                                        "X/X_" + str(trial) + ".txt"))
-            network_output_at_X = torch.tensor(np.loadtxt(
-                results_folder + "network_output_at_X/network_output_at_X_" + str(trial) + ".txt"))
-            objective_at_X = torch.tensor(np.loadtxt(
-                results_folder + "objective_at_X/objective_at_X_" + str(trial) + ".txt"))
+            X = torch.tensor(np.loadtxt(results_folder + "X/X_" + str(trial) + ".txt"))
+            network_output_at_X = torch.tensor(
+                np.loadtxt(
+                    results_folder
+                    + "network_output_at_X/network_output_at_X_"
+                    + str(trial)
+                    + ".txt"
+                )
+            )
+            objective_at_X = torch.tensor(
+                np.loadtxt(
+                    results_folder
+                    + "objective_at_X/objective_at_X_"
+                    + str(trial)
+                    + ".txt"
+                )
+            )
 
             # Historical best observed objective values and running times
-            hist_best_obs_vals = list(np.loadtxt(
-                results_folder + "best_obs_vals_" + str(trial) + ".txt"))
-            runtimes = list(np.loadtxt(
-                results_folder + "runtimes/runtimes_" + str(trial) + ".txt"))
+            hist_best_obs_vals = list(
+                np.loadtxt(results_folder + "best_obs_vals_" + str(trial) + ".txt")
+            )
+            runtimes = list(
+                np.loadtxt(results_folder + "runtimes/runtimes_" + str(trial) + ".txt")
+            )
 
             # Current best observed objective value
             best_obs_val = torch.tensor(hist_best_obs_vals[-1])
@@ -61,10 +81,10 @@ def bofn_trial(
 
             # Initial evaluations
             X = generate_initial_design(
-                num_samples=n_init_evals, input_dim=input_dim, seed=trial)
+                num_samples=n_init_evals, input_dim=input_dim, seed=trial
+            )
             network_output_at_X = function_network(X)
-            objective_at_X = network_to_objective_transform(
-                network_output_at_X)
+            objective_at_X = network_to_objective_transform(network_output_at_X)
 
             # Current best objective value
             best_obs_val = objective_at_X.max().item()
@@ -77,7 +97,8 @@ def bofn_trial(
     else:
         # Initial evaluations
         X = generate_initial_design(
-            num_samples=n_init_evals, input_dim=input_dim, seed=trial)
+            num_samples=n_init_evals, input_dim=input_dim, seed=trial
+        )
         network_output_at_X = function_network(X)
         objective_at_X = network_to_objective_transform(network_output_at_X)
 
@@ -106,6 +127,7 @@ def bofn_trial(
             network_to_objective_transform=network_to_objective_transform,
             dag=dag,
             active_input_indices=active_input_indices,
+            batch_size=batch_size,
         )
         t1 = time.time()
         runtimes.append(t1 - t0)
@@ -114,13 +136,13 @@ def bofn_trial(
         network_output_at_new_x = function_network(new_x)
 
         # Evaluate objective at new point
-        objective_at_new_x = network_to_objective_transform(
-            network_output_at_new_x)
+        objective_at_new_x = network_to_objective_transform(network_output_at_new_x)
 
         # Update training data
         X = torch.cat([X, new_x], 0)
         network_output_at_X = torch.cat(
-            [network_output_at_X, network_output_at_new_x], 0)
+            [network_output_at_X, network_output_at_new_x], 0
+        )
         objective_at_X = torch.cat([objective_at_X, objective_at_new_x], 0)
 
         # Update historical best observed objective values
@@ -130,14 +152,25 @@ def bofn_trial(
 
         # Save data
         np.savetxt(results_folder + "X/X_" + str(trial) + ".txt", X.numpy())
-        np.savetxt(results_folder + "network_output_at_X/network_output_at_X_" +
-                   str(trial) + ".txt", network_output_at_X.numpy())
-        np.savetxt(results_folder + "objective_at_X/objective_at_X_" +
-                   str(trial) + ".txt", objective_at_X.numpy())
-        np.savetxt(results_folder + "best_obs_vals_" +
-                   str(trial) + ".txt", np.atleast_1d(hist_best_obs_vals))
-        np.savetxt(results_folder + "runtimes/runtimes_" +
-                   str(trial) + ".txt", np.atleast_1d(runtimes))
+        np.savetxt(
+            results_folder
+            + "network_output_at_X/network_output_at_X_"
+            + str(trial)
+            + ".txt",
+            network_output_at_X.numpy(),
+        )
+        np.savetxt(
+            results_folder + "objective_at_X/objective_at_X_" + str(trial) + ".txt",
+            objective_at_X.numpy(),
+        )
+        np.savetxt(
+            results_folder + "best_obs_vals_" + str(trial) + ".txt",
+            np.atleast_1d(hist_best_obs_vals),
+        )
+        np.savetxt(
+            results_folder + "runtimes/runtimes_" + str(trial) + ".txt",
+            np.atleast_1d(runtimes),
+        )
 
 
 def get_new_suggested_point(
@@ -148,6 +181,7 @@ def get_new_suggested_point(
     network_to_objective_transform: Callable,
     dag: DAG,
     active_input_indices: List[int],
+    batch_size: int,
 ) -> Tensor:
     input_dim = X.shape[-1]
 
@@ -155,8 +189,12 @@ def get_new_suggested_point(
         return torch.rand([1, input_dim])
     elif algo == "EIFN":
         # Model
-        model = GaussianProcessNetwork(train_X=X, train_Y=network_output_at_X, dag=dag,
-                          active_input_indices=active_input_indices)
+        model = GaussianProcessNetwork(
+            train_X=X,
+            train_Y=network_output_at_X,
+            dag=dag,
+            active_input_indices=active_input_indices,
+        )
         # Sampler
         qmc_sampler = SobolQMCNormalSampler(num_samples=128)
         # Acquisition function
@@ -165,7 +203,6 @@ def get_new_suggested_point(
             best_f=objective_at_X.max().item(),
             sampler=qmc_sampler,
             objective=network_to_objective_transform,
-
         )
         posterior_mean_function = PosteriorMean(
             model=model,
@@ -180,7 +217,6 @@ def get_new_suggested_point(
             best_f=objective_at_X.max().item(),
             sampler=qmc_sampler,
             objective=network_to_objective_transform,
-
         )
         posterior_mean_function = PosteriorMean(
             model=model,
@@ -190,19 +226,20 @@ def get_new_suggested_point(
     elif algo == "EI":
         model = fit_gp_model(X=X, Y=objective_at_X)
         acquisition_function = ExpectedImprovement(
-            model=model, best_f=objective_at_X.max().item())
+            model=model, best_f=objective_at_X.max().item()
+        )
         posterior_mean_function = GPPosteriorMean(model=model)
     elif algo == "KG":
         model = fit_gp_model(X=X, Y=objective_at_X)
-        acquisition_function = qKnowledgeGradient(
-            model=model, num_fantasies=8)
+        acquisition_function = qKnowledgeGradient(model=model, num_fantasies=8)
         posterior_mean_function = GPPosteriorMean(model=model)
 
     new_x = optimize_acqf_and_get_suggested_point(
         acq_func=acquisition_function,
-        bounds=torch.tensor([[0. for i in range(input_dim)], [
-                            1. for i in range(input_dim)]]),
-        batch_size=1,
+        bounds=torch.tensor(
+            [[0.0 for i in range(input_dim)], [1.0 for i in range(input_dim)]]
+        ),
+        batch_size=batch_size,
         posterior_mean=posterior_mean_function,
     )
 
